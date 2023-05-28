bc657cbafee82fc37ef60c598dc8d57a1996bc87
bulicnik <bulicnik@gmail.com>
2023-05-28 13:07:27 -0500

    sprite update

--- Gotling/Assets/Scripts/Weapons/Weapon Base/ProjectileWeaponBehaviour.cs
+++ Gotling/Assets/Scripts/Weapons/Weapon Base/ProjectileWeaponBehaviour.cs
@@ -82,6 +82,14 @@ public class ProjectileWeaponBehaviour : MonoBehaviour
             enemy.TakeDamage(currentDamage);
             ReducePierce();
         }
+        else if(col.CompareTag("Prop"))
+        {
+            if (col.gameObject.TryGetComponent(out BreakableProps breakable))
+            {
+                breakable.TakeDamage(currentDamage);
+                ReducePierce();
+            }
+        }
     }
     void ReducePierce()
     {
