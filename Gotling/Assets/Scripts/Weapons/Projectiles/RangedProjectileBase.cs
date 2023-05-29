fa23125910e8219c4939851071117fe8ceed548c
markeuli <ulicnikmark24@gmail.com>
2023-05-28 23:29:38 -0500

    Added direction to fireball sprite

--- Gotling/Assets/Scripts/Weapons/Projectiles/RangedProjectileBase.cs
+++ Gotling/Assets/Scripts/Weapons/Projectiles/RangedProjectileBase.cs
@@ -35,47 +35,8 @@ public abstract class RangedProjectileBase : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         body.velocity = direction * weaponData.Speed;
 
-        float dirx = direction.x;
-        float diry = direction.y;
-
-        Vector3 scale = transform.localScale;
-        Vector3 rotation = transform.rotation.eulerAngles;
-
-        if (dirx < 0 && diry == 0) // left
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-        }
-        else if (dirx == 0 && diry < 0) // down
-        {
-            scale.y = scale.y * -1;
-        }
-        else if (dirx == 0 && diry > 0) // up
-        {
-            scale.x = scale.x * -1;
-        }
-        else if (dir.x > 0 && dir.y > 0) // right up
-        {
-            rotation.z = 0f;
-        }
-        else if (dir.x > 0 && dir.y < 0) // right down
-        {
-            rotation.z = -90f;
-        }
-        else if (dir.x <0 && dir.y > 0) //left up
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-            rotation.z = -90f;
-        }
-        else if (dir.x < 0 && dir.y <0) // left down
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-            rotation.z = 0f;
-        }
-        transform.localScale = scale;
-        transform.rotation = Quaternion.Euler(rotation);
+        
+        transform.right = -dir;
     }
     protected virtual void OnTriggerEnter2D(Collider2D col)
     {
